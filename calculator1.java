091903851e5f90baad644aa136c94066acdbf12d
NGUYEN THAI BAO <93515806+BaoBG@users.noreply.github.com>
2022-08-12 09:23:35 +0700

    Update calculator1.java

--- calculator1.java
+++ calculator1.java
@@ -1,1 +1,30 @@
+package calculator1;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		System.setIn(new FileInputStream("calculator1"));
+		Scanner sc = new Scanner(System.in);
+
+		for (int tc = 1; tc <= 10; tc++) {
+			int n = sc.nextInt();
+			String m = sc.nextLine();
+			String chuoi = "";
+			chuoi = sc.next();
+			int sum = 0;	
+			for (int i = 0; i < chuoi.length(); i++) {
+					if (i % 2 == 0) {
+						sum += Integer
+								.parseInt(String.valueOf(chuoi.charAt(i)));
+					}
+			}
+			System.out.println("#" + tc + " " + sum);
+
+		}
+	}
+
+}
