1b35a8f94ee395beeec785126bc7d1c93fbb44ae
NGUYEN THAI BAO <93515806+BaoBG@users.noreply.github.com>
2022-08-12 08:18:39 +0700

    Update string1.java
    đếm số lần xuất hiện của chuỗi trong câu. ví dụ: "ao" trong câu "Tao da bao may bao nhieu lan roi" => count = 3

--- string1.java
+++ string1.java
@@ -1,1 +1,36 @@
+package string;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		System.setIn(new FileInputStream("string"));
+		Scanner sc = new Scanner(System.in);
+
+		for (int tc = 1; tc <= 10; tc++) {
+			int index = sc.nextInt(); // doc chi so
+			String mString = sc.nextLine();
+			String chuoi = sc.nextLine(); // doc chuoi can tim
+			//sc.nextLine();
+			String cau = sc.nextLine();
+			
+			int count = 0;
+			int do_dai = chuoi.length();
+			for(int i = 0; i <= cau.length()- do_dai; i++){
+				String temp = "";
+				for(int j = i; j < i+do_dai; j++){
+					temp += String.valueOf(cau.charAt(j));
+				}
+				if(temp.equals(chuoi)){
+					count++;
+				}
+			}
+			
+			System.out.println("#" + index + " " +count);
+		}
+	}
+
+}
